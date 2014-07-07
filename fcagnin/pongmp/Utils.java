2ab171592230e397e475fd50cb5832c38d7419a9
Francesco Cagnin <francesco.cagnin@gmail.com>
2014-07-07 15:01:35 +0200

    fixed wrong randomFloat()

--- fcagnin/pongmp/Utils.java
+++ fcagnin/pongmp/Utils.java
@@ -2,6 +2,6 @@ package pongmp;
 
 public class Utils {
     public static float randomFloat(float min, float max) {
-        return (float) (Math.random() * (max - min) + max);
+        return (float) (Math.random() * (max - min) + min);
     }
 }
