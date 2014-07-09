45f98166301430696fec63284745b227ffccc0c1
Francesco Cagnin <francesco.cagnin@gmail.com>
2014-07-09 17:26:15 +0200

    moved compress() and decompress() methods to Utils

--- fcagnin/pongmp/Packet.java
+++ fcagnin/pongmp/Packet.java
@@ -2,13 +2,10 @@ package pongmp;
 
 import pongmp.entities.Ball;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.HashMap;
 import java.util.zip.DataFormatException;
-import java.util.zip.Deflater;
-import java.util.zip.Inflater;
 
 
 public class Packet {
@@ -31,7 +28,7 @@ public class Packet {
         }
 
         try {
-            return compress( byteBuffer.array() );
+            return Utils.compress( byteBuffer.array() );
         } catch ( IOException e ) {
             e.printStackTrace();
             return null;
@@ -43,7 +40,7 @@ public class Packet {
 
         ByteBuffer byteBuffer = null;
         try {
-            byteBuffer = ByteBuffer.wrap( decompress( bytes ) );
+            byteBuffer = ByteBuffer.wrap( Utils.decompress( bytes ) );
         } catch ( IOException e ) {
             e.printStackTrace();
             return null;
@@ -67,44 +64,6 @@ public class Packet {
         return packet;
     }
 
-
-    public static byte[] compress(byte[] data) throws IOException {
-        Deflater deflater = new Deflater();
-        deflater.setInput( data );
-
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream( data.length );
-
-        deflater.finish();
-        byte[] buffer = new byte[1024];
-        while ( !deflater.finished() ) {
-            int count = deflater.deflate( buffer ); // returns the generated code... index
-            outputStream.write( buffer, 0, count );
-        }
-        outputStream.close();
-        byte[] output = outputStream.toByteArray();
-
-        //        LOG.debug("Original: " + data.length / 1024 + " Kb");
-        //        LOG.debug("Compressed: " + output.length / 1024 + " Kb");
-        return output;
-    }
-
-    public static byte[] decompress(byte[] data) throws IOException, DataFormatException {
-        Inflater inflater = new Inflater();
-        inflater.setInput( data );
-
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream( data.length );
-        byte[] buffer = new byte[1024];
-        while ( !inflater.finished() ) {
-            int count = inflater.inflate( buffer );
-            outputStream.write( buffer, 0, count );
-        }
-        outputStream.close();
-        byte[] output = outputStream.toByteArray();
-
-        //        LOG.debug("Original: " + data.length);
-        //        LOG.debug("Compressed: " + output.length);
-        return output;
-    }
 
     ////////////////////////////////
 
