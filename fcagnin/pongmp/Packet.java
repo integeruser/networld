101cee03c743938711566b9e026e6f33bc8fb964
Francesco Cagnin <francesco.cagnin@gmail.com>
2014-07-09 14:46:39 +0200

    added packet compression using Inflater

--- fcagnin/pongmp/Packet.java
+++ fcagnin/pongmp/Packet.java
@@ -2,8 +2,13 @@ package pongmp;
 
 import pongmp.entities.Ball;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.HashMap;
+import java.util.zip.DataFormatException;
+import java.util.zip.Deflater;
+import java.util.zip.Inflater;
 
 
 public class Packet {
@@ -25,13 +30,27 @@ public class Packet {
             byteBuffer.put( Ball.serialize( ball ) );
         }
 
-        return byteBuffer.array();
+        try {
+            return compress( byteBuffer.array() );
+        } catch ( IOException e ) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     public static Packet deserialize(byte[] bytes) {
         Packet packet = new Packet();
 
-        ByteBuffer byteBuffer = ByteBuffer.wrap( bytes );
+        ByteBuffer byteBuffer = null;
+        try {
+            byteBuffer = ByteBuffer.wrap( decompress( bytes ) );
+        } catch ( IOException e ) {
+            e.printStackTrace();
+            return null;
+        } catch ( DataFormatException e ) {
+            e.printStackTrace();
+            return null;
+        }
 
         packet.serverTime = byteBuffer.getLong();
         packet.clientTime = System.nanoTime();
@@ -46,6 +65,45 @@ public class Packet {
         }
 
         return packet;
+    }
+
+
+    public static byte[] compress(byte[] data) throws IOException {
+        Deflater deflater = new Deflater();
+        deflater.setInput( data );
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream( data.length );
+
+        deflater.finish();
+        byte[] buffer = new byte[1024];
+        while ( !deflater.finished() ) {
+            int count = deflater.deflate( buffer ); // returns the generated code... index
+            outputStream.write( buffer, 0, count );
+        }
+        outputStream.close();
+        byte[] output = outputStream.toByteArray();
+
+        //        LOG.debug("Original: " + data.length / 1024 + " Kb");
+        //        LOG.debug("Compressed: " + output.length / 1024 + " Kb");
+        return output;
+    }
+
+    public static byte[] decompress(byte[] data) throws IOException, DataFormatException {
+        Inflater inflater = new Inflater();
+        inflater.setInput( data );
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream( data.length );
+        byte[] buffer = new byte[1024];
+        while ( !inflater.finished() ) {
+            int count = inflater.inflate( buffer );
+            outputStream.write( buffer, 0, count );
+        }
+        outputStream.close();
+        byte[] output = outputStream.toByteArray();
+
+        //        LOG.debug("Original: " + data.length);
+        //        LOG.debug("Compressed: " + output.length);
+        return output;
     }
 
     ////////////////////////////////
