13448f4f36889c84d50b4f7ce4698b2017750537
Francesco Cagnin <francesco.cagnin@gmail.com>
2014-07-12 12:12:07 +0200

    added render(), update(), createRandom()

--- fcagnin/pongmp/World.java
+++ fcagnin/pongmp/World.java
@@ -12,7 +12,15 @@ public class World {
     }
 
     ////////////////////////////////
+    public void update(float dt) {
+        for ( Ball ball : balls.values() ) { ball.update( dt ); }
+    }
 
+    public void render() {
+        for ( Ball ball : balls.values() ) { ball.render(); }
+    }
+
+    ////////////////////////////////
     public static void serialize(World world, ByteBuffer byteBuffer) {
         for ( Ball ball : world.balls.values() ) {
             byteBuffer.put( (byte) 127 );  // ball code
@@ -27,6 +35,19 @@ public class World {
             byteBuffer.get();  // ball code
 
             Ball ball = Ball.deserialize( byteBuffer );
+            world.balls.put( ball.id, ball );
+        }
+
+        return world;
+    }
+
+
+    public static World createRandom() {
+        World world = new World();
+
+        int numObjs = Utils.randomInt( 20, 100 );
+        for ( int i = 0; i < numObjs; i++ ) {
+            Ball ball = Ball.createRandom();
             world.balls.put( ball.id, ball );
         }
 
