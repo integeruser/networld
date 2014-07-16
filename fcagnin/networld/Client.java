6ccb5ad1bd64ba229c1bfd21b36a365525f5fe63
Francesco Cagnin <francesco.cagnin@gmail.com>
2014-07-16 12:21:25 +0200

    separated Display and console; increased Display resolution to 400x400; minor tweaks

--- fcagnin/networld/Client.java
+++ fcagnin/networld/Client.java
@@ -7,6 +7,7 @@ import networld.simulation.World;
 import org.lwjgl.LWJGLException;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
 
 import javax.swing.*;
 import java.awt.*;
@@ -24,77 +25,75 @@ public class Client {
     public static void main(String[] args) throws LWJGLException, InterruptedException {
         Server.main( null );  // temporary
 
-        Client client = new Client();
+        Client client = new Client( 400, 400 );
         client.start();
+
+        System.exit( 0 );  // temporary: kill all threads
     }
 
     ////////////////////////////////
-    public Client() {
+    public Client(int displayWidth, int displayHeight) throws LWJGLException {
+        vsync = true;
+
+        // create console before Display to not get window focus
+        console = new Console();
+        Point consoleLocation = console.getLocation();
+        // move console adjacent to Display
+        console.setLocation( consoleLocation.x + displayWidth / 2 + console.getWidth() / 2, consoleLocation.y );
+
+        Display.setTitle( "networld" );
+        Display.setDisplayMode( new DisplayMode( displayWidth, displayHeight ) );
+        Display.setVSyncEnabled( vsync );
+        Display.create();
+
         snapshots = new ConcurrentLinkedQueue<>();
         world = new World();
     }
 
+    ////////////////////////////////
     public void start() {
         ExecutorService service = Executors.newFixedThreadPool( 2 );
-        service.submit( new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    int port = 1337;
-                    Socket socket = new Socket( "localhost", port );
-                    ObjectInputStream in = new ObjectInputStream( socket.getInputStream() );
+        service.submit( () -> {
+            try {
+                int port = 1337;
+                Socket socket = new Socket( "localhost", port );
+                ObjectInputStream in = new ObjectInputStream( socket.getInputStream() );
 
-                    Packet lastPacket = new Packet();
-                    while ( true ) {
-                        byte[] bytes = (byte[]) in.readObject();
-                        byte[] decompressedBytes = Utils.decompress( bytes );
+                Packet lastPacket = new Packet();
+                while ( true ) {
+                    byte[] bytes = (byte[]) in.readObject();
+                    byte[] decompressedBytes = Utils.decompress( bytes );
 
-                        byte[] orig = new byte[decompressedBytes.length];
-                        if ( prevBytes != null ) {
-                            for ( int i = 0; i < orig.length; i++ ) {
-                                orig[i] = (byte) (prevBytes[i] ^ decompressedBytes[i]);
-                            }
-                        } else {
-                            for ( int i = 0; i < orig.length; i++ ) {
-                                orig[i] = decompressedBytes[i];
-                            }
+                    byte[] orig = new byte[decompressedBytes.length];
+                    if ( prevBytes != null ) {
+                        for ( int i = 0; i < orig.length; i++ ) {
+                            orig[i] = (byte) (prevBytes[i] ^ decompressedBytes[i]);
                         }
-                        prevBytes = new byte[orig.length];
-                        for ( int i = 0; i < decompressedBytes.length; i++ ) {
-                            prevBytes[i] = orig[i];
-                        }
-
-                        ByteBuffer byteBuffer = ByteBuffer.wrap( orig );
-                        Packet packet = Packet.deserialize( byteBuffer );
-
-                        // reject late packets
-                        if ( lastPacket.serverTime < packet.serverTime ) {
-                            snapshots.add( packet );
-                            lastPacket = packet;
+                    } else {
+                        for ( int i = 0; i < orig.length; i++ ) {
+                            orig[i] = decompressedBytes[i];
                         }
                     }
-                } catch ( IOException | ClassNotFoundException | DataFormatException e ) {
-                    e.printStackTrace();
-                    System.exit( -1 );
+                    prevBytes = new byte[orig.length];
+                    for ( int i = 0; i < decompressedBytes.length; i++ ) {
+                        prevBytes[i] = orig[i];
+                    }
+
+                    ByteBuffer byteBuffer = ByteBuffer.wrap( orig );
+                    Packet packet = Packet.deserialize( byteBuffer );
+
+                    // reject late packets
+                    if ( lastPacket.serverTime < packet.serverTime ) {
+                        snapshots.add( packet );
+                        lastPacket = packet;
+                    }
                 }
+            } catch ( IOException | ClassNotFoundException | DataFormatException e ) {
+                e.printStackTrace();
+                System.exit( -1 );
             }
         } );
 
-
-        try {
-            Canvas lwjglCanvas = new Canvas();
-            lwjglCanvas.setPreferredSize( new Dimension( 300, 300 ) );
-            Display.setParent( lwjglCanvas );
-            Renderer renderer = new Renderer( lwjglCanvas );
-
-            Display.setVSyncEnabled( true );
-            Display.create();
-        } catch ( LWJGLException e ) {
-            e.printStackTrace();
-            System.exit( -1 );
-        }
-
-        glViewport( 0, 0, Display.getWidth(), Display.getHeight() );
 
         Packet startPacket = new Packet(), endPacket = null;
         while ( !Display.isCloseRequested() ) {
@@ -144,20 +143,19 @@ public class Client {
                 }
             } else { System.out.println( "endPacket null" ); }
 
-            // render
             if ( Display.wasResized() ) { glViewport( 0, 0, Display.getWidth(), Display.getHeight() ); }
 
             glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
             world.render();
+            Display.update();
 
             loops++;
             cumLoopTime += System.nanoTime() - time;
-            Display.update();
+
             Display.sync( 60 );
         }
 
         Display.destroy();
-        System.exit( -1 );
     }
 
     private void processInput() {
@@ -173,25 +171,21 @@ public class Client {
     }
 
     ////////////////////////////////
+    private final Console console;
+
     private final ConcurrentLinkedQueue<Packet> snapshots;
     private final World world;
 
     private byte[] prevBytes;
 
+    // stats / settings
     private boolean vsync;
-
-    // stats
     private long loops, cumLoopTime;
 
     ////////////////////////////////
-    private class Renderer extends JFrame {
-        private Renderer(Canvas lwjglCanvas) {
-            super( "networld" );
-
-            JPanel contentPane = new JPanel();
-            contentPane.setLayout( new BoxLayout( contentPane, BoxLayout.Y_AXIS ) );
-
-            contentPane.add( lwjglCanvas );
+    private class Console extends JFrame {
+        private Console() {
+            super( "networld-console" );
 
             JPanel consolePanel = new JPanel() {
                 @Override
@@ -215,15 +209,12 @@ public class Client {
             ScheduledExecutorService service = Executors.newScheduledThreadPool( 1 );
             service.scheduleAtFixedRate( (Runnable) consolePanel::repaint, 0, 1, TimeUnit.SECONDS );
 
-            contentPane.add( consolePanel );
+            getContentPane().add( consolePanel );
 
-            setContentPane( contentPane );
             setDefaultCloseOperation( WindowConstants.EXIT_ON_CLOSE );
             pack();
             setLocationRelativeTo( null );
             setVisible( true );
-
-            lwjglCanvas.requestFocus();
         }
     }
 }
