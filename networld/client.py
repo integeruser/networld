f0b470bbdd2b90513568b2f22c33df146418979c
Francesco Cagnin <francesco.cagnin@gmail.com>
2016-01-21 17:04:25 +0100

    added multithreaded receive/process packet

--- networld/client.py
+++ networld/client.py
@@ -1,6 +1,34 @@
+import collections
 import socket
 import sys
+import threading
+import time
 import zlib
+
+import networking as n
+
+
+def receive():
+    while True:
+        recv_data, addr = s.recvfrom(2048)
+        if addr != saddr:
+            continue
+
+        packet = bytearray(zlib.decompress(recv_data))
+        assert 4+1+1 <= len(packet) <= 1440
+
+        deque.append(packet)
+        print('Received %d bytes (decompressed: %d)' % (len(recv_data), len(packet)))
+
+
+def process():
+    while True:
+        while deque:
+            packet = deque.popleft()
+            packetid = n.r_int(packet)
+            print('Processed packetid %d' % packetid)
+        time.sleep(1)
+
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
@@ -15,11 +43,6 @@ if __name__ == '__main__':
     # send any message to server to start a connection
     s.sendto(b'\xde\xad\xbe\xef', saddr)
 
-    while True:
-        recv_data, addr = s.recvfrom(2048)
-        if addr != saddr:
-            continue
-
-        packet = zlib.decompress(recv_data)
-        print("Received %d bytes (decompressed: %d)" % (len(recv_data), len(packet)))
-        assert 4+1+1 <= len(packet) <= 1440
+    deque = collections.deque(maxlen=3)
+    threading.Thread(target=receive).start()
+    threading.Thread(target=process).start()
