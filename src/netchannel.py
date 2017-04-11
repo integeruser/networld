09f99f3457baa83b9f9bdfabc97bc348519a1802
Francesco Cagnin <francesco.cagnin@gmail.com>
2017-04-11 17:21:27 +0200

    Add send rate and send multiple messages per packet

--- src/netchannel.py
+++ src/netchannel.py
@@ -11,50 +11,72 @@ import networking as n
 logger = logging.getLogger(__name__)
 
 
-def pack(reliable, seq, ack, data):
-    msg = bytearray()
-    n.w_byte(msg, reliable)
-    n.w_int(msg, seq)
-    n.w_int(msg, ack)
-    n.w_int(msg, len(data))
-    n.w_blob(msg, data)
-    return msg
-
-
-def unpack(msg):
-    reliable = n.r_byte(msg)
-    seq = n.r_int(msg)
-    ack = n.r_int(msg)
-    data = n.r_blob(msg, n.r_int(msg))
-    return (reliable, seq, ack, data)
-
-
 class Connection:
-    def __init__(self, sock, cl_addr):
+    MAX_PACKET_LEN = 2048
+
+    def __init__(self, sock, cl_addr, send_rate):
         self.sock = sock
         self.cl_addr = cl_addr
+        self.send_rate = send_rate
 
         self.send_deque = collections.deque()
         self.recv_deque = collections.deque()
         threading.Thread(target=self._send, daemon=True).start()
         threading.Thread(target=self._recv, daemon=True).start()
 
-    def _send(self):
+    def _pack_messages(self):
+        packet = bytearray()
+        msg_count = 0
+
         while True:
             try:
                 msg = self.send_deque.popleft()
             except IndexError:
-                pass
+                break
             else:
-                n = self.sock.sendto(zlib.compress(msg), self.cl_addr)
-                assert n <= 2048
-            time.sleep(0.005)
+                if len(packet) + 2 + len(msg) < Connection.MAX_PACKET_LEN:
+                    n.w_short(packet, len(msg))
+                    n.w_blob(packet, msg)
+                    msg_count += 1
+                else:
+                    self.send_deque.appendleft(msg)
+                    break
+        return packet, msg_count
+
+    def _unpack_messages(self, packet):
+        msg_count = 0
+        while packet:
+            msg = n.r_blob(packet, n.r_short(packet))
+            self.recv_deque.append(msg)
+            msg_count += 1
+        return msg_count
+
+    def _send(self):
+        dt = 1. / self.send_rate
+        acc = 0
+        current_time = time.perf_counter()
+        while True:
+            new_time = time.perf_counter()
+            frame_time = new_time - current_time
+            current_time = new_time
+
+            acc += frame_time
+            while acc >= dt:
+                acc -= dt
+
+                packet, msg_count = self._pack_messages()
+                if packet:
+                    n = self.sock.sendto(zlib.compress(packet), self.cl_addr)
+                    assert n <= Connection.MAX_PACKET_LEN
+                    logger.debug('Sent to %s count=%d bytes=%d' % (self.cl_addr, msg_count, n))
 
     def _recv(self):
         while True:
-            recv_data, _ = self.sock.recvfrom(2048)
-            msg = bytearray(zlib.decompress(recv_data))
-            self.recv_deque.append(msg)
+            recv_data, _ = self.sock.recvfrom(Connection.MAX_PACKET_LEN)
+            packet = bytearray(zlib.decompress(recv_data))
+            msg_count = self._unpack_messages(packet)
+            logger.debug('Recv fr %s count=%d bytes=%d' % (self.cl_addr, msg_count, len(recv_data)))
+            time.sleep(0.005)
 
     def enqueue(self, msg):
         self.send_deque.append(msg)
@@ -64,8 +86,8 @@ class Connection:
 
 
 class NetChannel:
-    def __init__(self, sock, cl_addr, process_callback):
-        self.conn = Connection(sock, cl_addr)
+    def __init__(self, sock, cl_addr, send_rate, process_callback):
+        self.conn = Connection(sock, cl_addr, send_rate)
         self.process_callback = process_callback
 
         self.seq_count = itertools.count()
@@ -78,6 +100,24 @@ class NetChannel:
         self.rel_msg_to_send = None
         threading.Thread(target=self._send, daemon=True).start()
         threading.Thread(target=self._recv, daemon=True).start()
+
+    @staticmethod
+    def _pack_message(reliable, seq, ack, data):
+        msg = bytearray()
+        n.w_byte(msg, reliable)
+        n.w_int(msg, seq)
+        n.w_int(msg, ack)
+        n.w_short(msg, len(data))
+        n.w_blob(msg, data)
+        return msg
+
+    @staticmethod
+    def _unpack_message(msg):
+        reliable = n.r_byte(msg)
+        seq = n.r_int(msg)
+        ack = n.r_int(msg)
+        data = n.r_blob(msg, n.r_short(msg))
+        return (reliable, seq, ack, data)
 
     def _send(self):
         while True:
@@ -92,7 +132,7 @@ class NetChannel:
                 # send the reliable message
                 reliable, seq, ack, data = self.rel_msg_to_send
                 self.ack_to_recv = seq
-                msg = pack(reliable, seq, ack, data)
+                msg = NetChannel._pack_message(reliable, seq, ack, data)
                 self.conn.enqueue(msg)
 
             # send all unreliables messages
@@ -102,7 +142,7 @@ class NetChannel:
                 except IndexError:
                     break
                 else:
-                    msg = pack(reliable, seq, ack, data)
+                    msg = NetChannel._pack_message(reliable, seq, ack, data)
                     self.conn.enqueue(msg)
             time.sleep(0.005)
 
@@ -113,7 +153,7 @@ class NetChannel:
             except IndexError:
                 pass
             else:
-                reliable, seq, ack, data = unpack(msg)
+                reliable, seq, ack, data = NetChannel._unpack_message(msg)
                 self.process(reliable, seq, ack, data)
             time.sleep(0.005)
 
@@ -146,10 +186,16 @@ class NetChannel:
 
 
 if __name__ == '__main__':
+    import argparse
     import socket
     import threading
 
-    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--debug', action='store_const', dest='loglevel', const=logging.DEBUG)
+    args = parser.parse_args()
+
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(stream=sys.stdout, level=args.loglevel)
 
     def ack(netchannel):
         while True:
@@ -164,8 +210,10 @@ if __name__ == '__main__':
     cl_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     cl_sock.bind(cl_addr)
 
-    sv_netchannel = NetChannel(sv_sock, cl_addr, lambda _: None)
-    cl_netchannel = NetChannel(cl_sock, sv_addr, lambda _: None)
+    send_rate = 10
+    process_callback = lambda _: None
+    sv_netchannel = NetChannel(sv_sock, cl_addr, send_rate, process_callback)
+    cl_netchannel = NetChannel(cl_sock, sv_addr, send_rate, process_callback)
 
     threading.Thread(target=ack, daemon=True, args=[sv_netchannel]).start()
     threading.Thread(target=ack, daemon=True, args=[cl_netchannel]).start()
@@ -173,4 +221,4 @@ if __name__ == '__main__':
     for data in [b'a', b'b', b'c', b'd', b'e']:
         sv_netchannel.transmit(data, reliable=True)
         cl_netchannel.transmit(data, reliable=True)
-    time.sleep(3)
+    time.sleep(5)
